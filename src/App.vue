b04999fc1d8dc4be244ccd2f5a6d47f25b0ab432
HP :) <1014763456@qq.com>
2022-09-05 20:08:44 +0800

    feat: 测试

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,6 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <h1>123</h1>
+  <router-view />
 </template>
 
 <style lang="scss">
